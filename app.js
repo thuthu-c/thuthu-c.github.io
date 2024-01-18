5cf109fdf039b4cc73459de81dbf4290ad7c10fd
thuthu-c <thuannyba@gmail.com>
2024-01-18 00:59:15 -0300

    build: site structure

---
+++ app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Thuanny Albuquerque',
+        subtitle: 'Backend PHP/Laravel Developer',
+        message: 'Welcome to my website!'
+    }
+});
---
+++ mobile-navbar.js
@@ -0,0 +1,48 @@
+class MobileNavbar{
+    constructor(mobileMenu, nav, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.nav = document.querySelector(nav);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(); 
+    }
+
+    
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+              }s`);
+        });
+    }
+    
+    handleClick(){
+        this.nav.classList.toogle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+
+        }
+
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu", 
+    ".navbar-nav", 
+    ".navbar-nav li"
+) 
+
+mobileNavbar.init();
