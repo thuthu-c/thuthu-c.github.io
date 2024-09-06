ddb3848101ab9125ef0abe635e8bc488e4869998
thuthu-c <thuannyba@gmail.com>
2024-09-06 14:58:44 -0300

    feat: add the app.js file

---
+++ newFile.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Thuanny Albuquerque',
+        subtitle: 'Backend Developer',
+        message: 'Experienced backend developer specializing in PHP and Laravel.',
+        skills: [
+            { name: 'PHP', icon: 'path/to/php-icon.png' },
+            { name: 'Laravel', icon: 'path/to/laravel-icon.png' },
+            { name: 'MySQL', icon: 'path/to/mysql-icon.png' },
+            { name: 'RESTful APIs', icon: 'path/to/api-icon.png' },
+            { name: 'Git', icon: 'path/to/git-icon.png' }
+        ],
+        projects: [
+            { name: 'Project 1', image: 'path/to/project1-screenshot.jpg' },
+            { name: 'Project 2', image: 'path/to/project2-screenshot.jpg' }
+        ],
+        profilePicture: 'images/20240828_175546.jpg'
+    }
+});
