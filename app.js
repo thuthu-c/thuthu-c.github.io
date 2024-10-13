4f01bafd4cd44a427356bcde25db19b6c9ac49eb
thuts <thuannyba@gmail.com>
2024-10-13 13:48:58 -0300

    feat: add the pictures

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ new Vue({
     el: '#app',
     data: {
         title: 'Thuanny Albuquerque',
-        subtitle: 'Backend Developer',
-        message: 'Experienced backend developer specializing in PHP and Laravel.',
+        subtitle: 'Backend Developer and Data Scientist',
+        message: "Hello! I'm a passionate Back-End Developer and Data Scientist with expertise in Python, PHP, and a variety of frameworks such as Laravel, Django, and Flask. I have a strong foundation in designing and implementing scalable, efficient APIs (REST), working with SQL databases, and following best practices in version control with Git Flow.\n\nThroughout my career, I've been dedicated to delivering robust and efficient back-end solutions, always focusing on clean, maintainable code. My interest in data science has also led me to develop skills in data analysis and machine learning, allowing me to tackle complex data-driven problems with creativity and precision.\n\nWith a solid understanding of software architecture and a passion for learning new technologies, I strive to bridge the gap between data and decision-making, creating impactful solutions that drive business success.\n\nI'm constantly seeking opportunities to enhance my skills and contribute to meaningful projects. Let's connect!",
         skills: [
             { name: 'PHP', icon: 'path/to/php-icon.png' },
             { name: 'Laravel', icon: 'path/to/laravel-icon.png' },
